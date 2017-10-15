59f49dd6e51c2fc814f418d3c207a9cba2e4cbf9
ParanoiacMonkey <stefano.negrimerlo@protonmail.com>
2017-10-15 18:42:40 +0200

    October 15

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public float horizontalVelocity;
+	static GameObject player;
+	static public float playerHeight;
+	static public float playerWidth;
+
+	void Update() {
+		if (player != null) {
+			Rigidbody rb = player.GetComponent<Rigidbody> ();
+			float v0 = rb.velocity.x;
+			float v1 = horizontalVelocity * InputReceiver.horizontalAxis;
+			float m = rb.mass;
+			rb.AddForce (Vector3.right * m * (v1 - v0) / Time.deltaTime);
+		}
+	}
+
+	static public void setPlayer(GameObject newPlayer) {
+		player = newPlayer;
+		Vector3 playerSize = player.GetComponent<Renderer> ().bounds.size;
+		playerHeight = playerSize.y;
+		playerWidth = playerSize.x;
+	}
+	static public void removePlayer(GameObject oldPlayer) {
+		if (player == oldPlayer)
+			removePlayer ();
+	}
+	static public void removePlayer() {
+		player = null;
+		playerHeight = 0;
+		playerWidth = 0;
+	}
+}
---
+++ Assets/TabManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabManager : MonoBehaviour {
+	static GameObject tabManager;
+	static float tabI;
+	static float tabsN;
+	void Awake () {
+		tabManager = gameObject;
+	}
+	void Update () {
+		
+	}
+}
